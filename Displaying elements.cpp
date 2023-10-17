a76b37a1bf7775870a8b704703c269e8885a89b5
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-17 19:03:24 +0530

    Displaying elements.cpp

---
+++ Displaying elements.cpp
@@ -0,0 +1,13 @@
+# Code-Chef-in-cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+   string week[4] = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+  cout << week[ 2] << endl;
+  cout << week[ 3 ];
+
+  return 0;
+}
+
