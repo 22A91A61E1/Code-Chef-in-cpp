d693fb2e36b577193f6f43769723316c78b8ece1
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-18 20:48:42 +0530

    User input Loop.cpp

---
+++ User input Loop.cpp
@@ -0,0 +1,20 @@
+# Code-Chef-in-cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int num;
+  cin >> num; 
+  for(int i=0;i<num;i++)
+  {
+          cout<<i<<endl;
+          
+  }
+
+
+
+
+
+  return 0;
+}
