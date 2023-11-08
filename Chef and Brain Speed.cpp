1353d703f0e6cef0c3389ed9b81ef5a43e847e5a
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-08 11:36:52 +0530

    Chef and Brain Speed.cpp

---
+++ Chef and Brain Speed.cpp
@@ -0,0 +1,21 @@
+# Code-Chef-in-cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	int y;
+	cin>>x>>y;
+
+	if(x<y)
+	{
+	        cout<<"YES"<<endl;
+	}
+	else
+	{
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
