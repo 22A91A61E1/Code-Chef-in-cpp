0a3b5f143880e37cca35b9abeda329fbfbdcc1ed
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-10 23:30:30 +0530

    Alice and Marks.cpp

---
+++ Alice and Marks.cpp
@@ -0,0 +1,20 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	int y;
+	cin>>x;
+	cin>>y;
+	if(x>=2*y)
+	{
+	        cout<<"Yes"<<endl;
+	}
+	else 
+	{
+	        cout<<"No"<<endl;
+	}
+	return 0;
+}
