264d8d18d2f1bb2feb222bd1b79419b1a2d4dfa2
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-11 21:52:22 +0530

    Oneful Pairs.cpp

---
+++ Oneful Pairs.cpp
@@ -0,0 +1,21 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	int b;
+	cin>>a;
+	cin>>b;
+	if((a+b+(a*b))==111)
+	{
+	        cout<<"Yes"<<endl;
+	}
+	else
+	{
+	        cout<<"No"<<endl;
+	}
+	
+	return 0;
+}
