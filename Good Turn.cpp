55d4c0b0c28541a9572b600ce50f143613e2bd1c
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-13 23:35:25 +0530

    Good Turn.cpp

---
+++ Good Turn.cpp
@@ -0,0 +1,25 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+         int T;
+         cin>>T;
+         for(int i=0;i<T;i++)
+         {
+         int a;
+         int b;
+         cin>>a>>b;
+         if((a+b)>6)
+         {
+                 cout<<("YES")<<endl;
+         }
+         else
+         {
+                 cout<<("NO")<<endl;
+         }
+         }
+	return 0;
+}
