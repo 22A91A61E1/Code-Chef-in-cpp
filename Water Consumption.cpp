96dfed8b378a2c50d311523a2a4b9c0c5db9ee7a
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-14 22:04:36 +0530

    Water Consumption.cpp

---
+++ Water Consumption.cpp
@@ -0,0 +1,25 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+	        int X;
+	        cin>>X;
+	        if(X>=2000)
+	        {
+	                cout<<"YES"<<endl;
+	        }
+	        else
+	        {
+	                cout<<"NO"<<endl;
+	        }
+	}
+	
+	return 0;
+}
+
