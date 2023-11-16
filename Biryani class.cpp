39fd39969b2b050b38605a38caa9704000aee7a0
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-16 11:19:42 +0530

    Biryani class.cpp

---
+++ Biryani class.cpp
@@ -0,0 +1,15 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++) {
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x*y<<endl;
+	}
+	return 0;
+}
