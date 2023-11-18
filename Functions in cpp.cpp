97f708955e77df27f97db335d5994350f45c8399
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-18 21:58:40 +0530

    Functions in cpp.cpp

---
+++ Functions in cpp.cpp
@@ -0,0 +1,31 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int A, B;
+        cin >> A >> B;
+        
+        int C = A * A + 2 * A * B + B * B;
+        int D = A + B;
+        
+        cout << C << endl;
+        cout << D << endl;
+
+        cin >> A >> B;
+        
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        
+        cout << C << endl;
+        cout << D << endl;
+        cin >> A >> B;
+        
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        
+        cout << C << endl;
+        cout << D << endl;
+	return 0;
+}
