018a9dc91536ad328e96088829248c8cfa888bd5
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-11-21 22:16:40 +0530

    Who is taller.cpp

---
+++ Who is taller.cpp
@@ -0,0 +1,18 @@
+# Code-Chef-in-cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y)
+	    cout<<"A"<<endl;
+	    else
+	    cout<<"B"<<endl;
+	}
+	return 0;
+}
